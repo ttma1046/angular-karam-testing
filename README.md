f0613b5c16b8d57fedd0d4f4b6b9f403401c5d7e
Chandler Fang <chandlerfang@gmail.com>
2019-07-27 11:34:46 +1000

    chore(unit-tests): renaming.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularDemo
+# Angular Karam Testing
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-demo'),
+      dir: require('path').join(__dirname, './coverage/angular-karam-testing'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
